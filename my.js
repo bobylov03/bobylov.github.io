76cdd69bf677809e0af8ca98b3ed3a8ebc79b990
bobylov03 <bobylevvlad0310@icloud.com>
2024-03-06 17:30:54 +0100

    ds

--- my.js
+++ my.js
@@ -1,14 +1,14 @@
 let button = document.querySelector(".no")
 button.addEventListener("mouseover", function(){
-    let rights = Math.floor(Math.random()*70)
-    let tops = Math.floor(Math.random()*70)
+    let rights = Math.floor(Math.random()*100)
+    let tops = Math.floor(Math.random()*100)
     this.style.top=`${tops}px`
     this.style.right=`${rights}px`
  
 })
 button.addEventListener("click", function(){
-    let rights = Math.floor(Math.random()*70)
-    let tops = Math.floor(Math.random()*70)
+    let rights = Math.floor(Math.random()*100)
+    let tops = Math.floor(Math.random()*100)
     this.style.top=`${tops}px`
     this.style.right=`${rights}px`
  
